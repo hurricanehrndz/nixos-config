61fc8f93872c1d26cfcf4c6bd113bb501bce7692
Carlos Hernandez <chernand@yelp.com>
2022-05-22 22:43:10 -0600

    Tweak zsh plugins load order

--- users/profiles/zsh/default.nix
+++ users/profiles/zsh/default.nix
@@ -1,9 +1,9 @@
+{  pkgs, ... }:
+
 {
   programs.zsh = {
     enable = true;
     dotDir = ".config/zsh";
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
     defaultKeymap = "viins";
     autocd = true;
     history = {
@@ -12,13 +12,50 @@
       ignoreDups = true;
       ignorePatterns = [ "rm *" "pkill *" ];
     };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "sudo" ];
-    };
     plugins = [
-
+      {
+        name = "ohmyzsh-git";
+        file = "share/oh-my-zsh/plugins/git/git.plugin.zsh";
+        src = pkgs.oh-my-zsh;
+      }
+      {
+        name = "history-substring-search";
+        file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
+        src = pkgs.zsh-history-substring-search;
+      }
+      {
+        name = "autosuggestions";
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+        src = pkgs.zsh-autosuggestions;
+      }
+      {
+        name = "fast-syntax-highlighting";
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+        src = pkgs.zsh-fast-syntax-highlighting;
+      }
     ];
+    initExtraBeforeCompInit = ''
+      function prepend_sudo {
+        if [[ "$BUFFER" != su(do|)\ * ]]; then
+          BUFFER="sudo $BUFFER"
+          (( CURSOR += 5 ))
+        fi
+      }
+      zle -N prepend_sudo
+
+      autoload -U edit-command-line
+      zle -N edit-command-line
+
+      bindkey   -M   viins   '\C-X\C-S'      prepend_sudo
+      bindkey   -M   viins   '\C-Y'          autosuggest-accept
+      bindkey   -M   viins   '\C-K'          history-substring-search-up
+      bindkey   -M   viins   '\C-J'          history-substring-search-down
+      bindkey   -M   vicmd   'k'             history-substring-search-up
+      bindkey   -M   vicmd   'j'             history-substring-search-down
+      bindkey   -M   vicmd   '\C-X\C-E'      edit-command-line
+      bindkey   -M   viins   '\C-X\C-E'      edit-command-line
+
+    '';
   };
   programs.starship = {
     enable = true;
