dbfa7e287473949ff44e95f1520543ca79126381
Carlos Hernandez <chernand@yelp.com>
2022-05-23 20:33:13 -0600

    Add some more useful aliases

--- users/profiles/shellAliases/default.nix
+++ users/profiles/shellAliases/default.nix
@@ -4,5 +4,10 @@
     la = "exa -aalhF --group-directories-first --icons";
     lt = "exa --tree --icons -d -a --ignore-glob '**/.git'";
     tm = "tmux new-session -A -s main";
+    cat = "bat";
+    type = "type -a";
+    rg = "rg -i -L";
+    vimdiff = "nvim -d";
+    mkdir = "mkdir -p";
   };
 }
