c537f5196808c05c55fdafb22157229f636180dc
Carlos Hernandez <hurricane@hrndz.ca>
2022-06-05 16:16:34 -0600

    Add some age secrets, lucy host keys

--- hosts/nixos/Lucy/default.nix
+++ hosts/nixos/Lucy/default.nix
@@ -1,10 +1,24 @@
-{ suites, ... }:
+{ suites, self, ... }:
+
 {
   ### root password is empty by default ###
   imports = (with suites; base ++ mediaserver) ++ [ ./hardware-configuration.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  age.secrets = {
+    "hosts/lucy/ssh_host_ed25519_key" = {
+      file = "${self}/secrets/hosts/lucy/ssh_host_ed25519_key.age";
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      symlink = false;
+    };
+    "hosts/lucy/ssh_host_ed25519_key.pub" = {
+      file = "${self}/secrets/hosts/lucy/ssh_host_ed25519_key.pub.age";
+      path = "/etc/ssh/ssh_host_ed25519_key.pub";
+      symlink = false;
+    };
+  };
 
   # Lucy has no swap device
   zramSwap.enable = true;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,20 @@
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  machineKeys = {
+    lucy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF+1D/p54Xvp1lOrbl84UvY4VNtncU7SHCBdwXBCg2F";
+  };
+  userKeys = {
+    hurricane = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFyXsPXbWMk433W+o+VwH7PasFbReJAEjHxcgUKhJ4P";
+  };
+  servers = with machineKeys; [
+    lucy
+  ];
+  admins = with userKeys; [
+    hurricane
+  ];
+  allKeys = servers ++ admins;
 in
 {
-  "secret.age".publicKeys = allKeys;
+  "hosts/lucy/ssh_host_ed25519_key.age".publicKeys = allKeys;
+  "hosts/lucy/ssh_host_ed25519_key.pub.age".publicKeys = allKeys;
 }
