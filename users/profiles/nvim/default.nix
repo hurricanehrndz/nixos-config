599971add2eccfeecaca3b61818d911b60516cf9
Carlos Hernandez <hurricane@hrndz.ca>
2022-06-07 21:59:26 -0600

    Add ripgrep and more keybind

--- users/profiles/nvim/default.nix
+++ users/profiles/nvim/default.nix
@@ -6,6 +6,9 @@
     vimdiffAlias = true;
     vimAlias = true;
     viAlias = true;
+    extraPackages = with pkgs; [
+      ripgrep
+    ];
     plugins = with pkgs.vimPlugins; [
       {
         plugin = tokyonight-nvim;
@@ -34,8 +37,23 @@
           local telescope = require("telescope")
           telescope.load_extension("fzf")
 
+          -- search for current word under cursor
+          vim.keymap.set("n", "<space>fw", function()
+            return require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
+          end)
+          vim.keymap.set("n", "<space>fs", require("telescope.builtin").grep_string)
+          vim.keymap.set("n", "<space>fg", require("telescope.builtin").live_grep)
+
+
           vim.keymap.set("n", "<C-p>", require("telescope.builtin").git_files)
           vim.keymap.set("n", "<space>ff", require("telescope.builtin").find_files)
+
+          -- buffer finder
+          vim.keymap.set("n", "<space>fb", require("telescope.builtin").buffers)
+
+          -- help finder
+          vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
+
         '';
       }
       plenary-nvim
