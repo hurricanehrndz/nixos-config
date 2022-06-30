33d0c29ef62cfb396d48887f1e49dac64fb86a19
Carlos Hernandez <chernand@yelp.com>
2022-06-29 21:12:02 -0600

    Fix mount options

--- profiles/remote-fs/deepthought-media-rw.nix
+++ profiles/remote-fs/deepthought-media-rw.nix
@@ -7,6 +7,11 @@
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
-      [ "${automount_opts}","credentials=${config.age.secrets."deepthought-rw.creds".path},uid=1000,gid=100" ];
+      [
+        "${automount_opts}"
+        ''credentials=${config.age.secrets."deepthought-rw.creds".path}''
+        "uid=1000"
+        "gid=100"
+      ];
   };
 }
