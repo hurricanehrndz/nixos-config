0eb6ec167097bf591a1f47301a061916132ceea8
Carlos Hernandez <hurricane@hrndz.ca>
2022-08-03 19:55:30 -0600

    Update toggleterm settings

--- users/profiles/nvim/config/lua/hrndz/plugins/toggleterm.lua
+++ users/profiles/nvim/config/lua/hrndz/plugins/toggleterm.lua
@@ -1,32 +1,42 @@
 require("toggleterm").setup({
   open_mapping = [[<c-\>]],
   hide_numbers = true,
-  direction = 'float',
+  direction = "horizontal",
+  size = 15,
 })
 
 vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=wipe'"
-vim.env.VISUAL = "nvr --remote-tab-wait +'set bufhidden=wipe'"
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen",
-  { clear = true }
-)
+local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = { "term://*" },
   callback = function()
-    local opts = {noremap = true}
-    vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-h>', [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-j>', [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-k>', [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-l>', [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-[>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-l>", [[<C-\><C-n><C-W>l]], opts)
   end,
   group = term_open_group,
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  count = 99,
+  hidden = true,
+  direction = "float",
+  close_on_exit = true,
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+})
 
 function _LAZYGIT_TOGGLE()
   lazygit:toggle()
 end
+
+vim.api.nvim_set_keymap("n", "<C-g>", [[<cmd>lua _LAZYGIT_TOGGLE()<CR>]], { noremap = true })
