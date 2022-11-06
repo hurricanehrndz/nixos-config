2328771341f7a3fa8191d3deaa8a5f5dde7e7b9e
Carlos Hernandez <carlos@hrndz.ca>
2022-11-06 09:30:07 -0700

    Rekey secrets with new deepthought key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   # set ssh public keys here for your system and user
   machineKeys = {
     lucy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF+1D/p54Xvp1lOrbl84UvY4VNtncU7SHCBdwXBCg2F";
-    deepthought = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrflPmpRjf6LsSggNI6h/B8xtIzZd+/SMjrkg9dj97a";
+    deepthought = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OP9bpbUbe4TWX9zRs2Yg4t3VY2Ef8GkohWvO6m/Aw";
   };
   userKeys = {
     hurricane = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFyXsPXbWMk433W+o+VwH7PasFbReJAEjHxcgUKhJ4P";
