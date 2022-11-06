5bb1e6bb4790484f76f1ec1eb56f68c39d621279
Carlos Hernandez <carlos@hrndz.ca>
2022-11-05 20:26:41 -0600

    Lucy no longer a service host

--- hosts/nixos/Lucy/default.nix
+++ hosts/nixos/Lucy/default.nix
@@ -2,8 +2,7 @@
 
 {
   ### root password is empty by default ###
-  imports = (with suites;
-    base ++ services-host) ++ [ ./hardware-configuration.nix ];
+  imports = (with suites; base) ++ [ ./hardware-configuration.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
