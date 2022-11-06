cc4c04405ef1b53e7e99fdbd7eb6a130bbe00f40
Carlos Hernandez <carlos@hrndz.ca>
2022-11-06 13:44:34 -0700

    Add some more sensible tmux configs

--- users/profiles/tmux/default.nix
+++ users/profiles/tmux/default.nix
@@ -7,15 +7,47 @@
     baseIndex = 1;
     keyMode = "vi";
     terminal = "tmux-256color";
+    aggressiveResize = true;
     escapeTime = 10;
     extraConfig = ''
       set-option -sa terminal-overrides ',*256col*:RGB'
       bind r source-file $HOME/.config/tmux/tmux.conf \; display "TMUX conf reloaded!"
+
+      # begin selection with v, yank with y
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+      bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h'  'select-pane -L'
+      bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j'  'select-pane -D'
+      bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k'  'select-pane -U'
+      bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l'  'select-pane -R'
+
+      bind-key -T copy-mode-vi 'M-h' select-pane -L
+      bind-key -T copy-mode-vi 'M-j' select-pane -D
+      bind-key -T copy-mode-vi 'M-k' select-pane -U
+      bind-key -T copy-mode-vi 'M-l' select-pane -R
+
+      # easily rotate window
+      bind-key -n 'M-o' rotate-window
+
+      # easily zoom
+      bind-key -n 'M-z' resize-pane -Z
     '';
     plugins = with pkgs; [
       {
         plugin = tmuxPlugins.power-theme;
         extraConfig = "set -g @themepack 'powerline/block/orange'";
+      }
+      {
+        plugin = tmuxPlugins.extrakto;
+        extraConfig = ''
+          set -g @extrakto_clip_tool_run "tmux_osc52"
+          set -g @extrakto_clip_tool "auto"
+          set -g @extrakto_popup_size "65%"
+          set -g @extrakto_grab_area "window 500"
+        '';
       }
     ];
   };
