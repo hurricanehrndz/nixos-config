8188fa14ff6d11bce61b346966eeafcbff24cf05
Carlos Hernandez <carlos@hrndz.ca>
2022-12-05 12:35:28 -0700

    Shutdown plex gracefully

--- profiles/plex/default.nix
+++ profiles/plex/default.nix
@@ -1,6 +1,22 @@
+{ pkgs, lib, config, ... }:
+
+let
+  pidFile = "${config.services.plex.dataDir}/Plex Media Server/plexmediaserver.pid";
+in
 {
   services.plex = {
     enable = true;
     openFirewall = true;
+  };
+  systemd.services.plex.serviceConfig = {
+    KillSignal = lib.mkForce "SIGKILL";
+    TimeoutStopSec = 10;
+    ExecStop = pkgs.writeScript "plex-stop" ''
+      #!${pkgs.bash}/bin/bash
+
+      ${pkgs.procps}/bin/pkill --signal 15 --pidfile "${pidFile}"
+      ${pkgs.coreutils}/bin/sleep 5
+    '';
+    PIDFile = lib.mkForce "";
   };
 }
